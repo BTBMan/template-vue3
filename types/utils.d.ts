f469320cd4cdd03f391ba571654be5dcb60bb697
BTBMan <1498118157@qq.com>
2022-08-29 10:17:10 +0800

    chore: utils.d.ts -> type-utils.d.ts
    Signed-off-by: BTBMan <1498118157@qq.com>

--- types/utils.d.ts
+++
@@ -1,7 +0,0 @@
-type RequiredKeys<T> = keyof {
-  [P in keyof T as Omit<T, P> extends T ? never : P]: T[P];
-};
-
-type IsUnion<A, B = A> = A extends A ? ([B] extends [A] ? false : true) : false;
-
-type HasRequired<T> = IsUnion<RequiredKeys<T> | ''>;
